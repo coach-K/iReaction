4fb7912ccb0b3ebb15d83603623ae9f6b8ae8cf9
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-16 13:01:03 +0100

    Create Test for Pair Class

---
+++ iReactionTest/Pair/PairTest.java
@@ -0,0 +1,32 @@
+package Pair;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PairTest {
+
+    Pair pair;
+
+    @Before
+    public void setUp() throws Exception {
+        pair = new Pair<String, String>("Name", "Ogunde Kehinde");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetKey() throws Exception {
+        assertEquals(pair.getKey(), "Name");
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+        assertEquals(pair.getValue(), "Ogunde Kehinde");
+    }
+}
