b46c87690e00b843921b2fec39710280f1ef8e10
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-17 13:22:11 +0100

    Refactor Pair Class Java Doc

---
+++ iReactionTest/Pair/PairListTest.java
@@ -0,0 +1,100 @@
+package Pair;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PairListTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testGet1() throws Exception {
+
+    }
+
+    @Test
+    public void testSize() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testContains1() throws Exception {
+
+    }
+
+    @Test
+    public void testIterator() throws Exception {
+
+    }
+
+    @Test
+    public void testToArray() throws Exception {
+
+    }
+
+    @Test
+    public void testToArray1() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testContainsAll() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveAll() throws Exception {
+
+    }
+
+    @Test
+    public void testRetainAll() throws Exception {
+
+    }
+
+    @Test
+    public void testClear() throws Exception {
+
+    }
+}
--- src/Pair/Pair.java
+++ src/Pair/Pair.java
@@ -2,23 +2,25 @@ package Pair;
 
 /**
  * @author OGUNDE KEHINDE
- * This class is a generics class with two types
- * {@literal} <K> key of element
- * {@literal} <V> value of element
- * <b>An Example code</b>
- * {@code} Pair<String, String> pair = new Pair<>("Hello", "World");
+ *         This class is a generics class with two types
+ *         {@literal} <K> key of element
+ *         {@literal} <V> value of element
+ *         <b>An Example code</b>
+ *         {@code} Pair<String, String> pair = new Pair<>("Hello", "World");
  */
-public class Pair <K, V> {
+public class Pair<K, V> {
     public static final String DEFAULT = "";
+
     private K key;
     private V value;
 
     /**
      * Accepts two parameter of types
-     * {@literal} <K>
-     * {@literal} <V>
-     * @param key type of element
-     * @param value value of element
+     * {@literal} <K> of element
+     * {@literal} <V> of element
+     *
+     * @param key   key to be stored
+     * @param value value to be stored
      */
     public Pair(K key, V value) {
         this.key = key;
