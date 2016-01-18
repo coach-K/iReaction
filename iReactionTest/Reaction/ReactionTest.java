13a35bf8233e833da1fc323dd48828b42c4abe43
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-18 19:11:56 +0100

    Add and Pass Reaction Class Test [Finishes #111770958]

--- iReactionTest/Reaction/ReactionTest.java
+++ iReactionTest/Reaction/ReactionTest.java
@@ -1,16 +1,27 @@
 package Reaction;
 
+import Pair.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class ReactionTest {
 
+    Reaction reaction;
+
+    Pair pair0 = createPair("UNIQUE-ID", "ANDELA-FELLOW");
+    Pair pair1 = createPair("GRACE", "OMOTOSO");
+    Pair pair2 = createPair("GENDER", "FEMALE");
+    Pair pair3 = createPair("SCHOOL", "ANDELA");
+    Pair pair4 = createPair("CLASS", "10");
+
     @Before
     public void setUp() throws Exception {
-
+        reaction = new Reaction();
     }
 
     @After
@@ -18,8 +29,25 @@ public class ReactionTest {
 
     }
 
+    public Pair<String, ArrayList<String>> createPair(String key, String value){
+        ArrayList<String> values = new ArrayList<>();
+        values.add(value);
+        return new Pair<>(key, values);
+    }
+
+    public void populatePairList(){
+        reaction.add(pair0);
+        reaction.add(pair1);
+        reaction.add(pair2);
+        reaction.add(pair3);
+        reaction.add(pair4);
+    }
+
     @Test
     public void testGetUniqueId() throws Exception {
-
+        assertEquals(reaction.size(), 0);
+        populatePairList();
+        assertNotNull(reaction);
+        assertEquals(reaction.getUniqueId(), "[ANDELA-FELLOW]");
     }
 }
