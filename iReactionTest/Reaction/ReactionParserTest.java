0721d7603d976ad052efc7b6917d917ca48b7e08
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-19 10:02:52 +0100

    Add ReactionPairParserTest class

---
+++ iReactionTest/Reaction/ReactionParserTest.java
@@ -0,0 +1,25 @@
+package Reaction;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReactionParserTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testReadReactant() throws Exception {
+
+    }
+}
