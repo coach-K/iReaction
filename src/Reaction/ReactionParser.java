fe7be81e58f628bade68d41d6359cd37cbae884e
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-19 09:21:12 +0100

    Add ReactionParser Class [Starts #111771972]

---
+++ src/Reaction/ReactionParser.java
@@ -0,0 +1,18 @@
+package Reaction;
+
+/**
+ * This class uses a PairFileParser
+ * to read the content of a pair file into a reaction object.
+ * returns a reaction element.
+ * This class is a member of the
+ * <a href="{@docRoot}/../src/parser/PairFileParser/index.html">
+ * Java Reader Framework</a>.
+ *
+ * @author OGUNDE KEHINDE
+ * @see Pair.Pair
+ * @see java.util.List
+ * @see java.util.ArrayList
+ * @since 1.7
+ */
+public class ReactionParser {
+}
