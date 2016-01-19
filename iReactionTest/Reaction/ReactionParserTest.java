fa0d3ed33d982886c48788fc8a2797f556fab415
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-19 15:28:40 +0100

    Add and Pass ReactionParser Test

--- iReactionTest/Reaction/ReactionParserTest.java
+++ iReactionTest/Reaction/ReactionParserTest.java
@@ -4,13 +4,22 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ReactionParserTest {
 
+    ReactionParser reactionParser;
+
     @Before
     public void setUp() throws Exception {
+        String DESKTOP_PATH = System.getProperty("user.dir");
+        String FILE_NAME = DESKTOP_PATH + "\\assets\\react.txt";
 
+        File file = new File(FILE_NAME);
+        reactionParser = new ReactionParser(file);
     }
 
     @After
@@ -20,6 +29,13 @@ public class ReactionParserTest {
 
     @Test
     public void testReadReactant() throws Exception {
-
+        Reaction reaction;
+        while ((reaction = reactionParser.readReaction()) != null) {
+            if (reactionParser.isEndOfBlock()) {
+                break;
+            }
+        }
+        assertNotNull(reaction);
+        assertEquals(reaction.getUniqueId(), "[RXN0-6375]");
     }
 }
