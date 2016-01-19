cb3400d4fd008ae7c0a63c58023c9f710a06df60
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-19 23:47:30 +0100

    Add Constants to Config Enum

--- src/Database/Config.java
+++ src/Database/Config.java
@@ -1,7 +1,22 @@
 package Database;
 
-/**
- * Created by OGUNDE KEHINDE on 1/19/2016.
- */
-public class Config {
+
+public enum Config {
+
+    USERNAME("root"),
+    PASSWORD("codeKenn"),
+    URL("jdbc:mysql://localhost:3306"),
+    DATABASE("reactiondb"),
+    TABLE("reactions");
+
+    private String CONSTANT;
+
+    private Config(String CONSTANT){
+        this.CONSTANT = CONSTANT;
+    }
+
+    @Override
+    public String toString() {
+        return CONSTANT;
+    }
 }
