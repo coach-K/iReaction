7143459958a706a9f5b43e8ec9c597ff6ac76eb7
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-19 16:12:17 +0100

    Add Database Connector Test [Starts #111784958]

---
+++ iReactionTest/Database/DatabaseConnectorTest.java
@@ -0,0 +1,30 @@
+package Database;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseConnectorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testConnect() throws Exception {
+
+    }
+
+    @Test
+    public void testDisconnect() throws Exception {
+
+    }
+}
