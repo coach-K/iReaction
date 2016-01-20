ff5115e38d934586bf1530401db8900ef80f0ccb
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-20 01:14:37 +0100

    Add QueryBuilder class

---
+++ iReactionTest/Database/QueryBuilderTest.java
@@ -0,0 +1,100 @@
+package Database;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueryBuilderTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testDatabase() throws Exception {
+
+    }
+
+    @Test
+    public void testTable() throws Exception {
+
+    }
+
+    @Test
+    public void testAddColumn() throws Exception {
+
+    }
+
+    @Test
+    public void testAddColumn1() throws Exception {
+
+    }
+
+    @Test
+    public void testAddColumn2() throws Exception {
+
+    }
+
+    @Test
+    public void testBuild() throws Exception {
+
+    }
+
+    @Test
+    public void testSelect() throws Exception {
+
+    }
+
+    @Test
+    public void testSelect1() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert1() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testWhere() throws Exception {
+
+    }
+
+    @Test
+    public void testAnd() throws Exception {
+
+    }
+
+    @Test
+    public void testWhereLike() throws Exception {
+
+    }
+
+    @Test
+    public void testAndLike() throws Exception {
+
+    }
+}
