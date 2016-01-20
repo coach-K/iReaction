365850e7a6892d2c8d0585dc168a9555c0ffa497
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-20 01:28:54 +0100

    Refactor Config clas

--- src/Database/Config.java
+++ src/Database/Config.java
@@ -1,16 +1,42 @@
 package Database;
 
-
+/**
+ * This class stores a MySQL credential constants
+ */
 public enum Config {
 
+    /**
+     * Username to MySQL database
+     */
     USERNAME("root"),
+
+    /**
+     * Password to MySQL database
+     */
     PASSWORD("codeKenn"),
+
+    /**
+     * Location to MySQL database
+     */
     URL("jdbc:mysql://localhost:3306"),
+
+    /**
+     * MySQL database to establish connection
+     */
     DATABASE("reactiondb"),
+
+    /**
+     * MySQL table to query
+     */
     TABLE("reactions");
 
     private String CONSTANT;
 
+    /**
+     * Construct this class
+     *
+     * @param CONSTANT to be set
+     */
     private Config(String CONSTANT){
         this.CONSTANT = CONSTANT;
     }
