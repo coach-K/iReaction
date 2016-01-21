e513edbe2c09f7de5d54d5ca1d4a5bfb58397e3d
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-21 03:36:55 +0100

    Add FilePath class to manage file paths

---
+++ src/Constant/FilePath.java
@@ -0,0 +1,22 @@
+package Constant;
+
+/**
+ * This class stores file path.
+ */
+public enum FilePath {
+    REACTION_FILE_PATH(System.getProperty("user.dir") + "\\assets\\react.txt"),
+    REACT_FILE_PATH(System.getProperty("user.dir") + "\\assets\\react.txt"),
+    DIFFERENT_FILE_PATH(System.getProperty("user.dir") + "\\assets\\pairparsertest.txt"),
+    LOG_FILE_PATH(System.getProperty("user.dir") + "\\assets\\logging.txt");
+
+    private String PATH;
+
+    private FilePath(String PATH) {
+        this.PATH = PATH;
+    }
+
+    @Override
+    public String toString() {
+        return PATH;
+    }
+}
