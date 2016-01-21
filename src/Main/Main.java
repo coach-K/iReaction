ef704292e1857badf855f3af2449403b9dd3ec30
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-21 03:26:05 +0100

    Update Main class JavaDoc

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,13 +1,25 @@
 package Main;
 
+import Reaction.Thread.DatabaseThread;
+import Reaction.Thread.LogThread;
+import Reaction.Thread.ReactionThread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
+ * This class runs application logic
+ *
  * @author OGUNDE KEHINDE
- * This class runs all application logic
  */
 
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("Main Application");
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.submit(new ReactionThread());
+        executorService.submit(new DatabaseThread());
+        executorService.submit(new LogThread());
+        executorService.shutdown();
     }
 }
