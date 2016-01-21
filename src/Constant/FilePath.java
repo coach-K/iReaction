e97c76b731ee155115aa03e3bb2514d6a745269d
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-21 04:09:39 +0100

    Update FilePath class [Finishes #112111107]

--- src/Constant/FilePath.java
+++ src/Constant/FilePath.java
@@ -4,7 +4,7 @@ package Constant;
  * This class stores file path.
  */
 public enum FilePath {
-    REACTION_FILE_PATH(System.getProperty("user.dir") + "\\assets\\react.txt"),
+    REACTION_FILE_PATH(System.getProperty("user.dir") + "\\assets\\reactions.dat"),
     REACT_FILE_PATH(System.getProperty("user.dir") + "\\assets\\react.txt"),
     DIFFERENT_FILE_PATH(System.getProperty("user.dir") + "\\assets\\pairparsertest.txt"),
     LOG_FILE_PATH(System.getProperty("user.dir") + "\\assets\\logging.txt");
