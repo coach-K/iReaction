6c0c35894936f78a24fcba06d07b4afef6b4621d
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-21 03:31:57 +0100

    Add Test to fail LogWriter

---
+++ iReactionTest/Logger/LogWriterTest.java
@@ -0,0 +1,34 @@
+package Logger;
+
+import Constant.FilePath;
+import Database.Config;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class LogWriterTest {
+
+    LogWriter logWriter;
+
+    @Before
+    public void setUp() throws Exception {
+        logWriter = new LogWriter(new File(FilePath.LOG_FILE_PATH.toString()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testWrite() throws Exception {
+        logWriter.write(Log.p(
+                this.getClass().getSimpleName(),
+                "wrote to file", System.currentTimeMillis()
+        ));
+    }
+}
