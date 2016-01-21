eaa1cd1660faa314e992ab0cbeaa30eefa3e2cff
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-01-21 03:32:52 +0100

    Add and Pass LogWriter Class Test

---
+++ src/Logger/Log.java
@@ -0,0 +1,67 @@
+package Logger;
+
+/**
+ * This class creates an element of a Log
+ */
+public class Log {
+
+    private String tag;
+    private String message;
+    private long date;
+
+    /**
+     * Construct this class with the specified element
+     * Accepts tags for message log
+     * accept message to be formatted
+     * accept date of the log
+     *
+     * @param tag     for message log
+     * @param message to be formatted
+     * @param date    of the log
+     */
+    public Log(String tag, String message, long date) {
+        this.tag = tag;
+        this.message = message;
+        this.date = date;
+    }
+
+    /**
+     * Returns new object with the specified arguments
+     * <p/>
+     * Accepts tags for message log
+     * accept message to be formatted
+     * accept date of the log
+     *
+     * @param tag     for message log
+     * @param message to be formatted
+     * @param date    of the log
+     * @return new object with the specified arguments
+     */
+    public static Log p(String tag, String message, long date) {
+        return new Log(tag, message, date);
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+}
---
+++ src/Logger/LogWriter.java
@@ -0,0 +1,53 @@
+package Logger;
+
+
+import Util.DateFormatter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Formatter;
+
+/**
+ * <p>{@code LogWriter} is meant for writing Log objects.
+ * <p/>
+ * An interpreter for printf-style format strings.  This class provides support
+ * for layout justification and alignment,
+ * writing to file and logging to console as output.
+ *
+ * @author OGUNDE KEHINDE
+ * @see Logger.Log
+ * @see java.util.Formatter
+ * @since 1.7
+ */
+public class LogWriter {
+
+    private Formatter formatter;
+
+    /**
+     * Construct this class with the specified argument
+     *
+     * @param file file path
+     * @throws IOException if file not found
+     */
+    public LogWriter(File file) throws IOException {
+        FileWriter fileWriter = new FileWriter(file, true);
+        this.formatter = new Formatter(fileWriter);
+    }
+
+    /**
+     * Writes the specified element to this file and console
+     *
+     * @param log to be written
+     */
+    public synchronized void write(Log log) {
+        String format = "%s %s - %s\r\n";
+        String tag = log.getTag();
+        String date = "(" + DateFormatter.getReadableDate(log.getDate()) + ")";
+        String message = log.getMessage();
+
+        this.formatter.format(format, tag, date, message);
+        this.formatter.flush();
+        System.out.printf(format, tag, date, message);
+    }
+}
